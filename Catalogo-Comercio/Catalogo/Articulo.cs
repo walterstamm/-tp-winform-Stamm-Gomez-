172fc7d0e2be2132a43859707b564f10d4655394
walterstamm <walterstamm92@gmail.com>
2021-04-23 15:49:53 -0300

    ...

---
+++ Catalogo-Comercio/Catalogo/Articulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogo
+{
+    class Articulo
+    {
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Marca { get; set; }
+        public string Categoria { get; set; }
+        public string Imagen { get; set; }///URL de imagen 
+        public float Precio { get; set; }
+
+    }
+}
--- Catalogo-Comercio/Catalogo/Form1.Designer.cs
+++ Catalogo-Comercio/Catalogo/Form1.Designer.cs
@@ -29,13 +29,47 @@ namespace Catalogo
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.LblFiltrar = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(619, 305);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // LblFiltrar
+            // 
+            this.LblFiltrar.AutoSize = true;
+            this.LblFiltrar.Location = new System.Drawing.Point(12, 26);
+            this.LblFiltrar.Name = "LblFiltrar";
+            this.LblFiltrar.Size = new System.Drawing.Size(32, 13);
+            this.LblFiltrar.TabIndex = 1;
+            this.LblFiltrar.Text = "Filtrar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblFiltrar);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label LblFiltrar;
     }
 }
 
