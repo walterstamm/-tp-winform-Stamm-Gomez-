3101425fee978693c1bafd62c0ed2af113837524
walterstamm <walterstamm92@gmail.com>
2021-04-24 02:11:51 -0300

    Botones agregados

--- Catalogo-Comercio/Catalogo/Form1.Designer.cs
+++ Catalogo-Comercio/Catalogo/Form1.Designer.cs
@@ -31,6 +31,11 @@ namespace Catalogo
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.LblFiltrar = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btnAgregarArticulos = new System.Windows.Forms.Button();
+            this.btnModificarArticulo = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnDetalles = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,11 +56,62 @@ namespace Catalogo
             this.LblFiltrar.TabIndex = 1;
             this.LblFiltrar.Text = "Filtrar";
             // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(66, 26);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(235, 27);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnAgregarArticulos
+            // 
+            this.btnAgregarArticulos.Location = new System.Drawing.Point(649, 84);
+            this.btnAgregarArticulos.Name = "btnAgregarArticulos";
+            this.btnAgregarArticulos.Size = new System.Drawing.Size(127, 34);
+            this.btnAgregarArticulos.TabIndex = 3;
+            this.btnAgregarArticulos.Text = "Agregar Articulos";
+            this.btnAgregarArticulos.UseVisualStyleBackColor = true;
+            // 
+            // btnModificarArticulo
+            // 
+            this.btnModificarArticulo.Location = new System.Drawing.Point(649, 142);
+            this.btnModificarArticulo.Name = "btnModificarArticulo";
+            this.btnModificarArticulo.Size = new System.Drawing.Size(127, 34);
+            this.btnModificarArticulo.TabIndex = 4;
+            this.btnModificarArticulo.Text = "Modificar";
+            this.btnModificarArticulo.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(649, 206);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(127, 34);
+            this.btnEliminar.TabIndex = 5;
+            this.btnEliminar.Text = "Eliminar ";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnDetalles
+            // 
+            this.btnDetalles.Location = new System.Drawing.Point(649, 267);
+            this.btnDetalles.Name = "btnDetalles";
+            this.btnDetalles.Size = new System.Drawing.Size(127, 34);
+            this.btnDetalles.TabIndex = 6;
+            this.btnDetalles.Text = "Detalles";
+            this.btnDetalles.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDetalles);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificarArticulo);
+            this.Controls.Add(this.btnAgregarArticulos);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.LblFiltrar);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
@@ -70,6 +126,11 @@ namespace Catalogo
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label LblFiltrar;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnAgregarArticulos;
+        private System.Windows.Forms.Button btnModificarArticulo;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnDetalles;
     }
 }
 
